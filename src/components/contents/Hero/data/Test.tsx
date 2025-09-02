ff7523d5dc018db48be5ec479e37fd147dda2848
csap <csirintronsopon8@gmail.com>
2025-09-02 18:05:50 +0700

    feat: new image and transition

--- src/components/contents/Hero/data/Test.tsx
+++
@@ -1,8 +0,0 @@
-export const Test = () => {
-    return (
-        <div>
-        <h1>Test Component</h1>
-        <p>This is a test component to verify the code structure.</p>
-        </div>
-    );
-    }
