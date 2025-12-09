c688fee5a221aefa42745dac2941115f64ce899e
csap <csirintronsopon8@gmail.com>
2025-12-10 03:53:51 +0700

    fix: scroll(vibe)

--- src/components/contents/About/AboutIntro.tsx
+++ src/components/contents/About/AboutIntro.tsx
@@ -6,38 +6,40 @@ import React, { useRef, useState } from "react";
 import styles from "./about.module.scss";
 
 interface Props {
-  childReal: React.ReactNode;
-  childIdeal: React.ReactNode;
+    childReal: React.ReactNode;
+    childIdeal: React.ReactNode;
 }
 
 export const AboutIntroduction = (props: Props) => {
-  const [isHover, setIsHover] = useState<Boolean>(false);
-  const containerRef = useRef<HTMLDivElement>(null);
+    const [isHover, setIsHover] = useState<Boolean>(false);
+    const containerRef = useRef<HTMLDivElement>(null);
 
-  const { x, y } = useMaskCursor(containerRef);
-  const { childReal, childIdeal } = props;
+    const { x, y } = useMaskCursor(containerRef);
+    const { childReal, childIdeal } = props;
 
-  const size = isHover ? 500 : 0;
+    const size = isHover ? 500 : 0;
 
-  return (
-    <div
-      ref={containerRef}
-      className="p-8 relative border border-gray-300 size-full"
-    >
-      <motion.div
-        className={styles.mask}
-        animate={{
-          maskPosition: `${x - size / 2}px ${y - size / 2}px`,
-          maskSize: `${size}px`,
-        }}
-        transition={{ type: "tween", ease: "backOut" }}
-        onMouseEnter={() => setIsHover(true)}
-        onMouseLeave={() => setIsHover(false)}
-      >
-        {childReal}
-      </motion.div>
+    return (
+        <div
+            ref={containerRef}
+            className="p-4 relative border border-gray-300 size-full"
+        >
+            <motion.div
+                className={styles.mask}
+                animate={{
+                    maskPosition: `${x - size / 2}px ${y - size / 2}px`,
+                    maskSize: `${size}px`,
+                }}
+                transition={{ type: "tween", ease: "backOut" }}
+                onMouseEnter={() => setIsHover(true)}
+                onMouseLeave={() => setIsHover(false)}
+            >
+                {childReal}
+            </motion.div>
 
-      <div className="size-full abosolute top-0 left-0 pb-8 ">{childIdeal}</div>
-    </div>
-  );
+            <div className="size-full abosolute top-0 left-0 pb-8 ">
+                {childIdeal}
+            </div>
+        </div>
+    );
 };
--- src/components/contents/Name/Name.tsx
+++ src/components/contents/Name/Name.tsx
@@ -1,63 +1,62 @@
-import { useEffect, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
 import { cn } from "src/lib/utils";
 import styles from "./name.module.scss";
 
 export const Name = () => {
-  const myName = ["CSAP", "Aungpao", "Chanatpakorn"];
-  const [name, setName] = useState(myName[0]);
-  const [index, setIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex(() => {
-        const newIndex = (index + 1) % myName.length;
-        setName(myName[newIndex]);
-        return newIndex;
-      });
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [myName]);
-
-  return (
-    <div className="flex flex-col items-center justify-center w-full py-16 text-4xl gap-4">
-      <div className="inline-flex items-center">
-        <img
-          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzcwN3BtNXg2Yzh0dGxhc3A3dzBzZG4wZTVwYWFkcmt4eHNiMm9iciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/OkoScrMcY324r1j1HZ/giphy.gif"
-          alt="dancing"
-          loading="eager"
-          className="hidden md:block translate-x-1/2"
-        />
-        <img
-          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzcwN3BtNXg2Yzh0dGxhc3A3dzBzZG4wZTVwYWFkcmt4eHNiMm9iciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/OkoScrMcY324r1j1HZ/giphy.gif"
-          alt="dancing"
-          loading="eager"
-        />
-        <img
-          src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzcwN3BtNXg2Yzh0dGxhc3A3dzBzZG4wZTVwYWFkcmt4eHNiMm9iciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/OkoScrMcY324r1j1HZ/giphy.gif"
-          alt="dancing"
-          loading="eager"
-          className="hidden md:block -translate-x-1/2"
-        />
-      </div>
-      <a
-        className="-translate-y-10 text-sm font-normal"
-        href="https://giphy.com/pixeljeff"
-      >
-        Thanks to this{" "}
-        <span className="underline underline-offset-1">artist</span>
-        <span className="inline md:hidden"> for a dancer</span>
-        <span className="md:inline hidden"> for dancers</span>
-      </a>
-      <p className="-translate-y-5">✨It's me!✨</p>
-      <p
-        className={cn(
-          "border-r-2 border-r-black  bg-spotlight p-4 text-white font-bold text-4xl",
-          styles.blinkingCursor,
-        )}
-      >
-        {name}
-      </p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center w-full py-8 text-4xl gap-4">
+            <div className="inline-flex items-center overflow-hidden place-content-center">
+                <img
+                    src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzcwN3BtNXg2Yzh0dGxhc3A3dzBzZG4wZTVwYWFkcmt4eHNiMm9iciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/OkoScrMcY324r1j1HZ/giphy.gif"
+                    alt="dancing"
+                    loading="eager"
+                    className="hidden md:block translate-x-1/2"
+                />
+                <img
+                    src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzcwN3BtNXg2Yzh0dGxhc3A3dzBzZG4wZTVwYWFkcmt4eHNiMm9iciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/OkoScrMcY324r1j1HZ/giphy.gif"
+                    alt="dancing"
+                    loading="eager"
+                />
+                <img
+                    src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzcwN3BtNXg2Yzh0dGxhc3A3dzBzZG4wZTVwYWFkcmt4eHNiMm9iciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/OkoScrMcY324r1j1HZ/giphy.gif"
+                    alt="dancing"
+                    loading="eager"
+                    className="hidden md:block -translate-x-1/2"
+                />
+            </div>
+            <a
+                className="-translate-y-10 text-sm font-normal"
+                href="https://giphy.com/pixeljeff"
+            >
+                Thanks to this{" "}
+                <span className="underline underline-offset-1">artist</span>
+                <span className="inline md:hidden"> for a dancer</span>
+                <span className="md:inline hidden"> for dancers</span>
+            </a>
+            <p className="-translate-y-5">✨It's me!✨</p>
+            <div
+                className={cn(
+                    " bg-spotlight p-4 text-white font-bold text-4xl flex flex-row",
+                )}
+            >
+                <p className="text-4xl">"</p>
+                <TypeAnimation
+                    sequence={[
+                        "CSAP",
+                        2000,
+                        "Aungpao",
+                        2000,
+                        "Chanatpakorn",
+                        2000,
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    repeat={Infinity}
+                    cursor={false}
+                    className={cn("border-r-4", styles.blinkingCursor)}
+                />
+                <p className={cn(styles.blinkingCursor, "text-4xl")}>"</p>
+            </div>
+        </div>
+    );
 };
